77f4c404c54ff88cd66ffb4a263944e26e0ccb8f
Carlos Diaz <hola@cjdiaz.com>
2019-05-25 04:59:41 -0500

    Adding new folder to test multi entry ponits

---
+++ multiple-entry-points/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, "index.js"),
+    precios: path.resolve(__dirname, "precios.js"),
+    contacto: path.resolve(__dirname, "contacto.js")
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      // Aqui van los loaders
+      {
+        // test: que tipo de archivo quero reconocer
+        // use: que loader se va a encargar del archivo
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+        // ["style-loader", "css-loader"]
+        // fallback:'style-loader',
+        // use: "css-loader"
+      }
+    ]
+  },
+  plugins: [
+    // aqui van los plugins
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css"
+    })
+  ]
+};
