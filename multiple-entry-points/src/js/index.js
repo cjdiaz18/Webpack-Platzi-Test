2df7835900743794610f8a32625c1986d8a82d6b
Carlos Diaz <hola@cjdiaz.com>
2019-05-25 05:12:31 -0500

    Configurating path fron each html file

---
+++ multiple-entry-points/src/js/index.js
@@ -0,0 +1,5 @@
+import "../css/estilos.css";
+
+document.write("Hola mundo, desde el home");
+//console.log("Hola mundo!, desde Webpack");
+console.log("Hola mundo!, desde Webpack en un webpack.config");
