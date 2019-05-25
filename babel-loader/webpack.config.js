58ccf588ab685a616217d3b29575bd3be661250d
Carlos Diaz <hola@cjdiaz.com>
2019-05-25 05:42:16 -0500

    Creating a new folder to test Babel

---
+++ babel-loader/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      // Aqui van los loaders
+      {
+        // test: que tipo de archivo quero reconocer
+        // use: que loader se va a encargar del archivo
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+        // ["style-loader", "css-loader"]
+        // fallback:'style-loader',
+        // use: "css-loader"
+      }
+    ]
+  },
+  plugins: [
+    // aqui van los plugins
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css"
+    })
+  ]
+};
