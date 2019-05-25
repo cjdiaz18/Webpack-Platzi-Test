e4de702304509cf21022d6e3fa4b63b29cb40866
Carlos Diaz <hola@cjdiaz.com>
2019-05-25 05:39:10 -0500

    Adding new module webpack-dev-server and editing package.json with new command build:dev to made automatic reload on html files

--- webpack-dev-server/index.js
+++ webpack-dev-server/index.js
@@ -1,5 +1,5 @@
 import "./estilos.css";
 
-document.write("Hola mundo");
+document.write("Hola Mundo Cruel :)");
 //console.log("Hola mundo!, desde Webpack");
 console.log("Hola mundo!, desde Webpack en un webpack.config");
